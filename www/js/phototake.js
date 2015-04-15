935d6e41959fbd5ec3c618cb857f5341dc0ff2fa
Paul Harvey <harv0116@algonquinlive.com>
2015-04-15 10:07:44 -0400

    2nd commit

---
+++ www/js/phototake.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+var take = {
+  	name: "Phone Final Project",
+  	version: "1.0",
+	
+    inittake: function() {
+       
+	   // change this to real functions
+	   // take.function();
+    }
+
+    
+};
+take.inittake();
